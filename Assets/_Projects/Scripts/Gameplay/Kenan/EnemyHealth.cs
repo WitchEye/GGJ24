b746b2409a8575b74ea9352e287c5548a42b846f
KMOzk <210372@student.glu.nl>
2024-01-26 10:55:42 +0100

    EnemyHealth + movement added

---
+++ Assets/_Projects/Scripts/Gameplay/Kenan/EnemyHealth.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int ScoreToAllocate;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(""))
+        {
+
+        }
+    }
+}
---
+++ Assets/_Projects/Scripts/Gameplay/Kenan/EnemyMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+[SelectionBase]
+public class EnemyMovement : MonoBehaviour
+{
+    SplineAnimate SplineToFollow;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
